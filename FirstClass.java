65d49a6fdc384657538c6c6995f0923a7243b602
akash patel <skypatel857@gmail.com>
2023-02-13 21:38:47 +0000

    first project

---
+++ FirstClass.java
@@ -0,0 +1,11 @@
+package firstpakage;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("akash");
+        System.out.println("patel");
+
+
+    }
+}
+
